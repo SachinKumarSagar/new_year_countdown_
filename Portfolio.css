2d7522e898b8ff781d54769c8db30760683c0852
SachinKumarSagar <ksachin22@iitk.ac.in>
2024-12-14 19:55:51 +0530

    Create Portfolio.css

---
+++ Portfolio.css
@@ -0,0 +1,16 @@
+// Tab switching logic
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (let tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (let tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    // Activate the clicked tab link and its corresponding content
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
